033cdb665e812323d90b9a51234d101e2c800833
Miroslav Pokrovskii <miroslav.pokrovskii@streamr.com>
2018-09-18 18:56:56 +0300

    Add version check in preinstall script

---
+++ src/check-versions.js
@@ -0,0 +1,25 @@
+const check = require('check-node-version')
+const { engines } = require('../package.json')
+
+check(
+    {
+        node: engines.node,
+        npm: engines.npm
+    },
+    (error, results) => {
+        if (error) {
+            console.error(error)
+            return
+        }
+
+        if (!results.isSatisfied) {
+            for (const packageName of Object.keys(results.versions)) {
+                if (!results.versions[packageName].isSatisfied) {
+                    console.error(`Required ${packageName} version ${engines[packageName]} not satisfied with current version ${results.versions[packageName].version.version}s\n\n`);
+                }
+            }
+
+            process.exit(1)
+        }
+    }
+)
