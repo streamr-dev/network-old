627ce556d057dbb9d6c0963ee850cf22598ab83b
Eric Andrews <eric.andrews@streamr.com>
2018-10-18 13:42:32 +0300

    fix ws-endpoint test

--- test/integration/ws-endpoint.test.js
+++ test/integration/ws-endpoint.test.js
@@ -9,7 +9,7 @@ describe('create five endpoints and init connection between them', () => {
     let promises = []
     const endpoints = []
 
-    it('should be able to start and stop successfully', async (done) => {
+    it('should be able to start and stop successfully', async () => {
         for (let i = 0; i < MAX; i++) {
             // eslint-disable-next-line no-await-in-loop
             const endpoint = await startEndpoint(LOCALHOST, 30690 + i, '', true).catch((err) => { throw err })
@@ -35,25 +35,15 @@ describe('create five endpoints and init connection between them', () => {
             promises.push(await waitForEvent(endpoints[i], endpointEvents.PEER_CONNECTED))
         }
 
-        Promise.all(promises)
-        //
-        // await wait(1000)
-        //
-        // for (let i = 0; i < MAX; i++) {
-        //     expect(endpoints[i].getPeers().size).toEqual(1)
-        // }
-        //
-        // promises = []
+        await Promise.all(promises)
+
+        for (let i = 0; i < MAX; i++) {
+            expect(endpoints[i].getPeers().size).toEqual(1)
+        }
+
         for (let i = 0; i < MAX; i++) {
             // eslint-disable-next-line no-await-in-loop
             await endpoints[i].stop(console.log(`closing ${i} endpoint`))
         }
-
-        done()
-
-        // TODO Unhandled promise rejection
-        // Promise.all(promises).then(() => {
-        //     done()
-        // }).catch((err) => { throw err })
     })
 })
