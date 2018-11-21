2b1a2970ef8c4147d8fd4e664bbc04d100ce5297
Eric Andrews <eric.andrews@streamr.com>
2018-11-21 18:40:13 +0200

    use ids on logic layer

--- src/protocol/PeerBook.js
+++ src/protocol/PeerBook.js
@@ -1,22 +1,48 @@
-const Endpoint = require('../connection/Endpoint')
-
 module.exports = class PeerBook {
-    constructor(endpoint) {
+    constructor() {
+        this.idToAddress = {}
+        this.idToType = {}
         this.addressToId = {}
-        this.addressToType = {}
-
-        endpoint.on(Endpoint.events.PEER_CONNECTED, (peerAddress, customHeaders) => {
-            this.addressToId[peerAddress] = customHeaders['streamr-peer-id']
-            this.addressToType[peerAddress] = customHeaders['streamr-peer-type']
-        })
-
-        endpoint.on(Endpoint.events.PEER_DISCONNECTED, (peerAddress) => {
-            delete this.addressToId[peerAddress]
-            delete this.addressToType[peerAddress]
-        })
     }
 
-    getShortId(address) {
+    add(peerAddress, metadata) {
+        const peerId = metadata['streamr-peer-id']
+        const peerType = metadata['streamr-peer-type']
+
+        this.idToAddress[peerId] = peerAddress
+        this.idToType[peerId] = peerType
+        this.addressToId[peerAddress] = peerId
+    }
+
+    remove(peerAddress) {
+        const peerId = this.addressToId[peerAddress]
+        delete this.idToAddress[peerId]
+        delete this.idToType[peerId]
+        delete this.addressToId[peerAddress]
+    }
+
+    getAddress(peerId) {
+        if (!this.hasPeerId(peerId)) {
+            throw new Error(`Id ${peerId} not found in peer book`)
+        }
+        return this.idToAddress[peerId]
+    }
+
+    getPeerId(address) {
+        if (!this.addressToId[address]) {
+            throw new Error(`Address ${address} not found in peer book`)
+        }
         return this.addressToId[address]
+    }
+
+    getType(peerId) {
+        if (!this.hasPeerId(peerId)) {
+            throw new Error(`Id ${peerId} not found in peer book`)
+        }
+        return this.idToType[peerId]
+    }
+
+    hasPeerId(address) {
+        return this.idToAddress[address] != null
     }
 }
