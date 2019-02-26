f83953d3afda4dd1a88b63356a5f81754e99d569
Eric Andrews <eric.andrews@streamr.com>
2019-02-26 11:24:22 +0200

    make instructions declarative

--- src/logic/OverlayTopology.js
+++ src/logic/OverlayTopology.js
@@ -27,7 +27,7 @@ module.exports = class OverlayTopology {
     }
 
     leave(nodeId) {
-        this.nodes[nodeId].forEach((neighbor) => this.nodes[neighbor].delete(nodeId)) // assumption: neighbor already initialized in tr
+        this.nodes[nodeId].forEach((neighbor) => this.nodes[neighbor].delete(nodeId)) // assumption: neighbor already initialized in tracker
         delete this.nodes[nodeId]
     }
 
@@ -42,58 +42,43 @@ module.exports = class OverlayTopology {
     formInstructions(nodeId) {
         const excessNeighbors = this.nodes[nodeId].size - this.maxNeighborsPerNode
         if (excessNeighbors > 0) {
-            const neighborsToDisconnectFrom = this.shuffleArray([...this.nodes[nodeId]]).slice(0, excessNeighbors)
-            return neighborsToDisconnectFrom.map((neighbor) => {
-                return {
-                    action: 'disconnect',
-                    source: nodeId,
-                    destination: neighbor,
-                }
-            })
+            return {
+                [nodeId]: this.shuffleArray([...this.nodes[nodeId]]).slice(0, this.maxNeighborsPerNode)
+            }
         }
 
-        let instructions = []
+        const instructions = {
+            [nodeId]: [...this.nodes[nodeId]]
+        }
         let missingNeighbors = -excessNeighbors
 
         if (missingNeighbors > 0) {
             const candidates = Object.entries(this.nodes)
-                .filter(([n, neighbors]) => neighbors.size < this.maxNeighborsPerNode) // there are open slots
-                .filter(([n, neighbors]) => !neighbors.has(nodeId)) // nodeId is not already a neighbor
-                .filter(([n, _]) => n !== nodeId) // do not connect to self
+                .filter(([n, neighbors]) => neighbors.size < this.maxNeighborsPerNode) // nodes with open slots
+                .filter(([n, neighbors]) => !neighbors.has(nodeId)) // nodes that are not already neighbors
+                .filter(([n, _]) => n !== nodeId) // remove self
                 .sort(([n1, neighbors1], [n2, neighbors2]) => neighbors1.size - neighbors2.size) // sort by reserved slots (ascending)
                 .map(([n, _]) => n)
 
-            const neighborsToConnectTo = this.shuffleArray(candidates).slice(0, missingNeighbors)
-            instructions = neighborsToConnectTo.map((n) => {
-                const r = this.random() < 0.5
-                return {
-                    action: 'connect',
-                    source: r ? nodeId : n,
-                    destination: r ? n : nodeId,
-                }
-            })
-            missingNeighbors -= instructions.length
+            const neighborsToAdd = this.shuffleArray(candidates).slice(0, missingNeighbors)
+            instructions[nodeId].push(...neighborsToAdd)
+            missingNeighbors -= neighborsToAdd.length
         }
 
         if (missingNeighbors > 0) {
             const candidates = Object.entries(this.nodes)
-                .filter(([n, neighbors]) => neighbors.size >= this.maxNeighborsPerNode) // there are no open slots
-                .filter(([n, neighbors]) => !neighbors.has(nodeId)) // nodeId is not already a neighbor
-                .filter(([n, _]) => n !== nodeId) // do not connect to self
+                .filter(([n, neighbors]) => neighbors.size >= this.maxNeighborsPerNode) // nodes with no open slots
+                .filter(([n, neighbors]) => !neighbors.has(nodeId)) // nodes that are not already neighbors
+                .filter(([n, _]) => n !== nodeId) // remove self
                 .map(([n, _]) => n)
 
             // TODO: if nodeId is already connected to one participant of a connection that will be disconnected, we will
             //  not be freeing up 2 slots but instead only 1. Should this be taken into consideration?
             const disconnectionTargets = this.shuffleArray(candidates).slice(0, Math.floor(missingNeighbors / 2))
-            instructions.push(...disconnectionTargets.map((n) => {
-                const r = this.random() < 0.5
+            disconnectionTargets.forEach((n) => {
                 const randomNeighbor = this.pickRandomElement([...this.nodes[n]])
-                return {
-                    action: 'disconnect',
-                    source: r ? n : randomNeighbor,
-                    destination: r ? randomNeighbor : n,
-                }
-            }))
+                instructions[n] = [...this.nodes[n]].filter((neighbor) => neighbor !== randomNeighbor)
+            })
         }
 
         return instructions
