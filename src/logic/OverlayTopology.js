8753e25533112c5c8e58ecbc08099c3bd2a19df7
Eric Andrews <eric.andrews@streamr.com>
2019-02-26 11:34:59 +0200

    make eslint happy

--- src/logic/OverlayTopology.js
+++ src/logic/OverlayTopology.js
@@ -1,4 +1,4 @@
-//https://gist.github.com/guilhermepontes/17ae0cc71fa2b13ea8c20c94c5c35dc4
+// From: https://gist.github.com/guilhermepontes/17ae0cc71fa2b13ea8c20c94c5c35dc4
 const shuffleArray = (arr) => arr
     .map((a) => [Math.random(), a])
     .sort((a, b) => a[0] - b[0])
