30708173c1522bfe124f2a0a2c1f7c929c592d06
Eric Andrews <eric.andrews@streamr.com>
2019-04-12 01:14:11 +0300

    test/util.js: waitForCondition
    A nifty little function that can be used to await for some condition to
    become true before proceeding with execution. One should usually prefer
    awaiting for events but this can be a good alternative when there are
    numerous events and complex interactions to deal with.

--- test/util.js
+++ test/util.js
@@ -10,6 +10,28 @@ const waitForEvent = (emitter, event, timeout = 20 * 1000) => pEvent(emitter, ev
     multiArgs: true
 })
 
+const waitForCondition = (fn, timeout = 10 * 1000, retryInterval = 100) => {
+    if (fn()) {
+        return Promise.resolve()
+    }
+    return new Promise((resolve, reject) => {
+        const refs = {}
+
+        refs.timeOut = setTimeout(() => {
+            clearInterval(refs.interval)
+            reject(new Error('waitForCondition: timed out before condition became true'))
+        }, timeout)
+
+        refs.interval = setInterval(() => {
+            if (fn()) {
+                clearTimeout(refs.timeOut)
+                clearInterval(refs.interval)
+                resolve()
+            }
+        }, retryInterval)
+    })
+}
+
 const getPeers = (max) => Array.from(Array(max), (d, i) => 'address-' + i)
 
 const eventsToArray = (emitter, events) => {
@@ -25,6 +47,7 @@ module.exports = {
     getPeers,
     wait,
     waitForEvent,
+    waitForCondition,
     DEFAULT_TIMEOUT,
     LOCALHOST,
 }
