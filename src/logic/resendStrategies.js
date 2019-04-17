28e10b2d5a3316feff47face83446d8cdaabf757
Eric Andrews <eric.andrews@streamr.com>
2019-04-17 17:42:24 +0300

    refactor: AskNeighborsResendStrategy
    Extract proxying behavior to its own class that is then used by
    AskNeighborsResendStrategy. This is in preparation for L3 which will
    have similar resend proxying logic.

--- src/logic/resendStrategies.js
+++ src/logic/resendStrategies.js
@@ -85,6 +85,117 @@ class StorageResendStrategy {
     }
 }
 
+class ProxiedResend {
+    constructor(request, responseStream, nodeToNode, getNeighbors, maxTries, timeout, onDoneCb) {
+        this.request = request
+        this.responseStream = responseStream
+        this.nodeToNode = nodeToNode
+        this.getNeighbors = getNeighbors
+        this.maxTries = maxTries
+        this.timeout = timeout
+        this.onDoneCb = onDoneCb
+        this.neighborsAsked = new Set()
+        this.currentNeighbor = null
+        this.timeoutRef = null
+
+        // Below are important for function identity in _detachEventHandlers
+        this._onUnicast = this._onUnicast.bind(this)
+        this._onResendResponse = this._onResendResponse.bind(this)
+        this._onNodeDisconnect = this._onNodeDisconnect.bind(this)
+    }
+
+    commence() {
+        this._attachEventHandlers()
+        this._askNextNeighbor()
+    }
+
+    cancel() {
+        this._endStream()
+    }
+
+    _attachEventHandlers() {
+        this.nodeToNode.on(NodeToNode.events.UNICAST_RECEIVED, this._onUnicast)
+        this.nodeToNode.on(NodeToNode.events.RESEND_RESPONSE, this._onResendResponse)
+        this.nodeToNode.on(NodeToNode.events.NODE_DISCONNECTED, this._onNodeDisconnect)
+    }
+
+    _detachEventHandlers() {
+        this.nodeToNode.removeListener(NodeToNode.events.UNICAST_RECEIVED, this._onUnicast)
+        this.nodeToNode.removeListener(NodeToNode.events.RESEND_RESPONSE, this._onResendResponse)
+        this.nodeToNode.removeListener(NodeToNode.events.NODE_DISCONNECTED, this._onNodeDisconnect)
+    }
+
+    _onUnicast(unicastMessage) {
+        const subId = unicastMessage.getSubId()
+        const source = unicastMessage.getSource()
+
+        if (this.request.getSubId() === subId && this.currentNeighbor === source) {
+            this.responseStream.push(unicastMessage)
+            this._resetTimeout()
+        }
+    }
+
+    _onResendResponse(response) {
+        const subId = response.getSubId()
+        const source = response.getSource()
+
+        if (this.request.getSubId() === subId && this.currentNeighbor === source) {
+            if (response instanceof ResendResponseResent) {
+                this._endStream()
+            } else if (response instanceof ResendResponseNoResend) {
+                this._askNextNeighbor()
+            } else if (response instanceof ResendResponseResending) {
+                this._resetTimeout()
+            } else {
+                throw new Error(`unexpected response type ${response}`)
+            }
+        }
+    }
+
+    _onNodeDisconnect(nodeId) {
+        if (this.currentNeighbor === nodeId) {
+            this._askNextNeighbor()
+        }
+    }
+
+    _askNextNeighbor() {
+        clearTimeout(this.timeoutRef)
+
+        if (this.neighborsAsked.size >= this.maxTries) {
+            this._endStream()
+            return
+        }
+
+        const candidates = this.getNeighbors(this.request.getStreamId()).filter((x) => !this.neighborsAsked.has(x))
+        if (candidates.length === 0) {
+            this._endStream()
+            return
+        }
+
+        const neighborId = candidates[0]
+        this.neighborsAsked.add(neighborId)
+
+        this.nodeToNode.send(neighborId, this.request).then(() => {
+            this.currentNeighbor = neighborId
+            this._resetTimeout()
+        }, () => {
+            this._askNextNeighbor()
+        })
+    }
+
+    _endStream() {
+        clearTimeout(this.timeoutRef)
+        this.responseStream.push(null)
+        this._detachEventHandlers()
+        this.onDoneCb()
+    }
+
+    _resetTimeout() {
+        clearTimeout(this.timeoutRef)
+        this.timeoutRef = setTimeout(this._askNextNeighbor.bind(this), this.timeout)
+    }
+}
+
 /**
  * Resend strategy that forwards resend request to neighbor nodes and then acts
  * as a proxy in between.
@@ -96,51 +207,7 @@ class AskNeighborsResendStrategy {
         this.getNeighbors = getNeighbors
         this.maxTries = maxTries
         this.timeout = timeout
-        this.pending = {}
-
-        this.nodeToNode.on(NodeToNode.events.UNICAST_RECEIVED, (unicastMessage) => {
-            const subId = unicastMessage.getSubId()
-            const source = unicastMessage.getSource()
-
-            if (this.pending[subId]) {
-                if (this.pending[subId].currentNeighbor === source) {
-                    this.pending[subId].responseStream.push(unicastMessage)
-                    this._reSetTimeout(subId)
-                } else {
-                    console.error(`received unicast from non-current neighbor ${source}`)
-                }
-            } else {
-                console.error(`received unicast for unknown subId ${subId}`)
-            }
-        })
-        this.nodeToNode.on(NodeToNode.events.RESEND_RESPONSE, (response) => {
-            const subId = response.getSubId()
-            const source = response.getSource()
-            if (this.pending[subId]) {
-                if (this.pending[subId].currentNeighbor === source) {
-                    if (response instanceof ResendResponseResent) {
-                        this._endStream(subId)
-                    } else if (response instanceof ResendResponseNoResend) {
-                        this._askNextNeighbor(subId)
-                    } else if (response instanceof ResendResponseResending) {
-                        this._reSetTimeout(subId)
-                    } else {
-                        throw new Error(`unexpected response type ${response}`)
-                    }
-                } else {
-                    console.error(`received resend response from non-current neighbor ${source}`)
-                }
-            } else {
-                console.error(`received resend response for unknown subId ${subId}`)
-            }
-        })
-        this.nodeToNode.on(NodeToNode.events.NODE_DISCONNECTED, (nodeId) => {
-            Object.entries(this.pending).forEach(([subId, { currentNeighbor }]) => {
-                if (currentNeighbor === nodeId) {
-                    this._askNextNeighbor(subId)
-                }
-            })
-        })
+        this.pending = new Set()
     }
 
     getResendResponseStream(request) {
@@ -151,14 +218,17 @@ class AskNeighborsResendStrategy {
 
         // L2 only works on local requests
         if (request.getSource() === null) {
-            this.pending[request.getSubId()] = {
-                responseStream,
+            const proxiedResend = new ProxiedResend(
                 request,
-                neighborsAsked: new Set(),
-                currentNeighbor: null,
-                timeoutRef: null
-            }
-            this._askNextNeighbor(request.getSubId())
+                responseStream,
+                this.nodeToNode,
+                this.getNeighbors,
+                this.maxTries,
+                this.timeout,
+                () => this.pending.delete(proxiedResend)
+            )
+            this.pending.add(proxiedResend)
+            proxiedResend.commence()
         } else {
             responseStream.push(null)
         }
@@ -167,46 +237,7 @@ class AskNeighborsResendStrategy {
     }
 
     stop() {
-        Object.keys(this.pending).forEach(this._endStream.bind(this))
-    }
-
-    _askNextNeighbor(subId) {
-        const { request, neighborsAsked, timeoutRef } = this.pending[subId]
-
-        clearTimeout(timeoutRef)
-
-        if (neighborsAsked.size >= this.maxTries) {
-            this._endStream(subId)
-            return
-        }
-
-        const candidates = this.getNeighbors(request.getStreamId()).filter((x) => !neighborsAsked.has(x))
-        if (candidates.length === 0) {
-            this._endStream(subId)
-            return
-        }
-
-        const neighborId = candidates[0]
-        neighborsAsked.add(neighborId)
-
-        this.nodeToNode.send(neighborId, request).then(() => {
-            this.pending[subId].currentNeighbor = neighborId
-            this._reSetTimeout(subId)
-        }, () => {
-            this._askNextNeighbor(subId)
-        })
-    }
-
-    _endStream(subId) {
-        const { responseStream, timeoutRef } = this.pending[subId]
-        clearTimeout(timeoutRef)
-        responseStream.push(null)
-        delete this.pending[subId]
-    }
-
-    _reSetTimeout(subId) {
-        clearTimeout(this.pending[subId].timeoutRef)
-        this.pending[subId].timeoutRef = setTimeout(() => this._askNextNeighbor(subId), this.timeout)
+        this.pending.forEach((proxiedResend) => proxiedResend.cancel())
     }
 }
 
