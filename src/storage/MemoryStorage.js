19232f78d557f68cfa350ef5194ae7e06afb3f63
Miroslav Pokrovskii <miroslavpokrovskiy@gmail.com>
2019-04-18 13:31:10 +0300

    First version of MemoryStorage

---
+++ src/storage/MemoryStorage.js
@@ -0,0 +1,136 @@
+const { Readable } = require('stream')
+
+module.exports = class MemoryStorage {
+    constructor() {
+        this.storage = new Map()
+        this.index = new Map()
+    }
+
+    store(streamId, streamPartition, subId, timestamp, sequenceNo, publisherId, msgChainId, data) {
+        const id = `${timestamp}, ${sequenceNo}, ${publisherId}, ${msgChainId}`
+        const index = this._index(streamId, streamPartition, subId)
+
+        if (!this.storage.has(index)) {
+            this.storage.set(index, {})
+        }
+
+        if (!this.index.has(index)) {
+            this.index.set(index, [])
+        }
+
+        this.storage.get(index)[id] = {
+            id, data
+        }
+
+        this.index.get(index)[timestamp] = id
+    }
+
+    _index(streamId, streamPartition, subId) {
+        return `${streamId}-${streamPartition}-${subId}`
+    }
+
+    hasStreamId(streamId, streamPartition, subId) {
+        const index = this._index(streamId, streamPartition, subId)
+
+        return this.storage.has(index) && this.index.has(index)
+    }
+
+    size(streamId, streamPartition, subId) {
+        const index = this._index(streamId, streamPartition, subId)
+
+        return this.hasStreamId(streamId, streamPartition, subId) ? Object.keys(this.storage.get(index)).length : 0
+    }
+
+    requestLast(streamId, streamPartition, subId, number) {
+        if (!Number.isInteger(number) || number <= 0) {
+            throw new TypeError('number is not an positive integer')
+        }
+
+        const index = this._index(streamId, streamPartition, subId)
+
+        const stream = new Readable({
+            objectMode: true
+        })
+
+        // eslint-disable-next-line no-underscore-dangle
+        stream._read = () => {
+            if (this.hasStreamId(streamId, streamPartition, subId)) {
+                const indexes = this.index.get(index).slice(-number)
+
+                indexes.forEach((id, timestamp) => {
+                    stream.push(this.storage.get(index)[id].data)
+                })
+            }
+
+            stream.push(null)
+        }
+
+        return stream
+    }
+
+    requestResendFrom(streamId, streamPartition, subId, fromTimestamp, fromSequenceNo = '', publisherId = '') {
+        if (!Number.isInteger(fromTimestamp) || fromTimestamp <= 0) {
+            throw new TypeError('fromTimestamp is not an positive integer')
+        }
+
+        const index = this._index(streamId, streamPartition, subId)
+
+        const stream = new Readable({
+            objectMode: true
+        })
+
+        // eslint-disable-next-line no-underscore-dangle
+        stream._read = () => {
+            if (this.hasStreamId(streamId, streamPartition, subId)) {
+                const indexes = this.index.get(index).filter((id, timestamp) => {
+                    return timestamp >= fromTimestamp
+                })
+
+                indexes.forEach((id, timestamp) => {
+                    stream.push(this.storage.get(index)[id].data)
+                })
+            }
+
+            stream.push(null)
+        }
+
+        return stream
+    }
+
+    requestResendRange(streamId, streamPartition, subId, fromTimestamp, toTimestamp, fromSequenceNo = 0, toSequenceNo = 0, publisherId = '') {
+        if (!Number.isInteger(fromTimestamp) || fromTimestamp <= 0) {
+            throw new TypeError('fromTimestamp is not an positive integer')
+        }
+
+        if (!Number.isInteger(toTimestamp) || toTimestamp <= 0) {
+            throw new TypeError('toTimestamp is not an positive integer')
+        }
+
+        if (fromTimestamp > toTimestamp) {
+            throw new TypeError('fromTimestamp must be less or equal than toTimestamp')
+        }
+
+        const index = this._index(streamId, streamPartition, subId)
+
+        const stream = new Readable({
+            objectMode: true
+        })
+
+        // eslint-disable-next-line no-underscore-dangle
+        stream._read = () => {
+            if (this.hasStreamId(streamId, streamPartition, subId)) {
+                const indexes = this.index.get(index).filter((id, timestamp) => {
+                    return timestamp >= fromTimestamp && timestamp <= toTimestamp
+                })
+
+                indexes.forEach((id, timestamp) => {
+                    stream.push(this.storage.get(index)[id].data)
+                })
+            }
+
+            stream.push(null)
+        }
+
+        return stream
+    }
+}
---
+++ test/unit/MemoryStorage.test.js
@@ -0,0 +1,149 @@
+const MemoryStorage = require('../../src/storage/MemoryStorage')
+const DataMessage = require('../../src/messages/DataMessage')
+const { StreamID, MessageID, MessageReference } = require('../../src/identifiers')
+
+const dataMessages = []
+const dataMessages2 = []
+const MAX = 100000
+const streamIdInit = 'stream-1'
+const streamIdInit2 = 'stream-2'
+const streamObj = new StreamID(streamIdInit, 0)
+const streamObj2 = new StreamID(streamIdInit2, 0)
+const subId = 0
+
+const { id, partition } = streamObj
+let memoryStorage
+
+for (let i = 0; i < MAX; i++) {
+    const dataMessage = new DataMessage(
+        new MessageID(streamObj, i, 0, 'publisher-id', 'sessionId'), i === 0 ? null : new MessageReference(i - 1, 0), {
+            messageNo: i
+        }
+    )
+    dataMessages.push(dataMessage)
+}
+
+for (let i = 0; i < MAX / 2; i++) {
+    const dataMessage = new DataMessage(
+        new MessageID(streamObj2, i, 0, 'publisher-id', 'sessionId'), i === 0 ? null : new MessageReference(i - 1, 0), {
+            messageNo: i
+        }
+    )
+    dataMessages2.push(dataMessage)
+}
+
+describe('test mem storage', () => {
+    beforeEach(() => {
+        memoryStorage = new MemoryStorage()
+
+        for (let i = 0; i < MAX; i++) {
+            const dataMessage = dataMessages[i]
+            const { timestamp, sequenceNo, publisherId, msgChainId } = dataMessage.getMessageId()
+
+            memoryStorage.store(id, partition, subId, timestamp, sequenceNo, publisherId, msgChainId, dataMessage.getData())
+        }
+    })
+
+    test('store data in memory storage', () => {
+        expect(memoryStorage.hasStreamId(id, partition, subId)).toBeTruthy()
+        expect(memoryStorage.size(id, partition, subId)).toBe(MAX)
+        expect(memoryStorage.hasStreamId(streamObj2.id, streamObj2.partition, subId)).toBeFalsy()
+    })
+
+    test('requestLast(10) should return 10 records', (done) => {
+        const RECORDS_NEEDED = 10
+        const last10Stream = memoryStorage.requestLast(id, partition, subId, RECORDS_NEEDED)
+
+        let arr = []
+
+        last10Stream.on('readable', () => {
+            while (true) {
+                const data = last10Stream.read()
+                if (data !== null) {
+                    arr.push(data)
+                } else {
+                    break
+                }
+            }
+        })
+
+        last10Stream.on('end', () => {
+            expect(arr.length).toEqual(RECORDS_NEEDED)
+
+            const data = dataMessages.slice(-RECORDS_NEEDED).map((dataMessage) => dataMessage.getData())
+
+            expect(arr.length).toEqual(RECORDS_NEEDED)
+            expect(arr).toEqual(data)
+            done()
+        })
+    })
+
+    test('test requestLast 0 and -1', () => {
+        try {
+            memoryStorage.requestLast(id, partition, subId, 0)
+        } catch (error) {
+            expect(error).toEqual(new TypeError('number is not an positive integer'))
+        }
+
+        try {
+            memoryStorage.requestLast(id, partition, subId, -1)
+        } catch (error) {
+            expect(error).toEqual(new TypeError('number is not an positive integer'))
+        }
+    })
+
+    test('test requestFrom', (done) => {
+        const FROM_TIME = 5
+        const fromStream = memoryStorage.requestResendFrom(id, partition, subId, FROM_TIME)
+
+        let arr = []
+
+        fromStream.on('readable', () => {
+            while (true) {
+                const data = fromStream.read()
+                if (data !== null) {
+                    arr.push(data)
+                } else {
+                    break
+                }
+            }
+        })
+
+        fromStream.on('end', () => {
+            const dataMessagesNeeded = dataMessages.filter((dataMessage) => dataMessage.getMessageId().timestamp >= FROM_TIME)
+            expect(arr.length).toEqual(dataMessagesNeeded.length)
+
+            const data = dataMessagesNeeded.map((dataMessage) => dataMessage.getData())
+            expect(arr).toEqual(data)
+            done()
+        })
+    })
+
+    test('test requestRange', (done) => {
+        const FROM_TIME = 5
+        const TO_TIME = 10
+        const fromStream = memoryStorage.requestResendRange(id, partition, subId, FROM_TIME, TO_TIME)
+
+        let arr = []
+
+        fromStream.on('readable', () => {
+            while (true) {
+                const data = fromStream.read()
+                if (data !== null) {
+                    arr.push(data)
+                } else {
+                    break
+                }
+            }
+        })
+
+        fromStream.on('end', () => {
+            const dataMessagesNeeded = dataMessages.filter((dataMessage) => dataMessage.getMessageId().timestamp >= FROM_TIME && dataMessage.getMessageId().timestamp <= TO_TIME)
+            expect(arr.length).toEqual(dataMessagesNeeded.length)
+
+            const data = dataMessagesNeeded.map((dataMessage) => dataMessage.getData())
+            expect(arr).toEqual(data)
+            done()
+        })
+    })
+})
