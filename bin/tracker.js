b4e936315230dfe513d6c03fb10a4a3f25885fcb
Eric Andrews <eric.andrews@streamr.com>
2019-10-31 09:20:54 +0200

    Fix Dockerfile

--- bin/tracker.js
+++ bin/tracker.js
@@ -3,12 +3,14 @@
 const { startTracker } = require('../src/composition')
 
 const port = process.argv[2] || 30300
-const ip = process.argv[3] || '127.0.0.1'
+const host = process.argv[3] || null
 const maxNeighborsPerNode = parseInt(process.argv[4], 10) || 4
 const id = `tracker-${port}`
 
 startTracker(ip, port, id, maxNeighborsPerNode)
-    .then(() => {})
+    .then(() => {
+        console.info('Tracker %s listening on %s:%d', id, ip, port)
+    })
     .catch((err) => {
         console.error(err)
         process.exit(1)
