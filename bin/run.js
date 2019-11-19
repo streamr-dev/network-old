974cef80b4b99d07b732c34402557204c5fa55e1
mirpo <miroslavpokrovskiy@gmail.com>
2019-11-19 15:07:19 +0200

    Update run.js script

--- bin/run.js
+++ bin/run.js
@@ -6,6 +6,7 @@ const path = require('path')
 const numberOfNodes = process.argv[2] || 10
 const startingPort = 30400
 const startingDebugPort = 9200
+const streams = ['stream1', 'stream2', 'stream3']
 let debug = false
 
 const productionEnv = Object.create(process.env)
@@ -14,7 +15,7 @@ productionEnv.checkUncaughtException = true
 
 // create tracker
 const tracker = path.resolve('./bin/tracker.js')
-let args = [tracker]
+let args = [tracker, '--endpointServerPort=11111']
 
 if (process.env.NODE_DEBUG_OPTION !== undefined) {
     debug = true
@@ -29,6 +30,7 @@ spawn('node', args, {
 for (let i = 0; i < numberOfNodes; i++) {
     args = [
         path.resolve('./bin/subscriber.js'),
+        '--streamId=' + streams[Math.floor(Math.random() * streams.length)],
         '--port=' + (startingPort + i)
     ]
 
