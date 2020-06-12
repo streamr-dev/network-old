b7ebe4de55fdcb3567c6f0d46046d661e18bd156
Eric Andrews <eric.andrews@streamr.com>
2020-06-12 17:12:42 +0300

    write json(s) to log file

--- webrtc/node.js
+++ webrtc/node.js
@@ -1,5 +1,7 @@
 const { EventEmitter } = require('events')
+const fs = require('fs')
 
+const tmp = require('tmp')
 const WebSocket = require('ws')
 const createDebug = require('debug')
 const { RTCPeerConnection, RTCSessionDescription } = require('wrtc')
@@ -11,6 +13,7 @@ program
     .option('--stun-urls <stun-urls>', 'comma-separated URL(s) of STUN servers', 'stun:stun.l.google.com:19302')
     .option('--publish-interval <publisher-interval>', 'interval in ms to publish', '500')
     .option('--report-interval <report-interval>', 'interval in ms to report', '30000')
+    .option('--log-file <log-file>', 'log file to use', null)
     .description('Run WebRTC example node')
     .parse(process.argv)
 
@@ -24,6 +27,7 @@ const { signaller } = program
 const stunUrls = program.stunUrls.split(',')
 const publishInterval = parseInt(program.publishInterval, 10)
 const reportInterval = parseInt(program.reportInterval, 10)
+const logFile = program.logFile ? program.logFile : tmp.fileSync().name
 const debug = createDebug('node.js')
 
 console.info('Node ID:', nodeId)
@@ -31,6 +35,11 @@ console.info('Using STUN URL(s):', stunUrls)
 console.info('Connecting to signaller', signaller)
 console.info('Publish interval ms: ', publishInterval)
 console.info('Report interval ms: ', reportInterval)
+console.info('Logging to file: ', logFile)
+
+const logFileStream = fs.createWriteStream(logFile, {
+    flags: 'a'
+})
 
 const connections = {}
 const dataChannels = {}
@@ -182,11 +191,12 @@ ws.on('open', () => {
 })
 
 ws.on('close', () => {
+    logFileStream.end()
     console.error('Connection to signaller dropped.')
     process.exit(1)
 })
 
-setInterval(() => {
+setInterval(async () => {
     console.info('Total messages received %d (%d)',
         numOfMessagesReceived,
         numOfMessagesReceived - lastReportedNumOfMessagesReceived)
@@ -199,6 +209,38 @@ setInterval(() => {
     console.info('Total bytes sent %d (%s per second)',
         numOfBytesSent,
         ((numOfBytesSent - lastReportedNumOfMessagesSent) / (reportInterval / 1000.0)).toFixed(2))
+    const connectionStats = await Promise.all(Object.values(connections).map((c) => c.getStats(null)))
+    logFileStream.write(JSON.stringify({
+        nodeId,
+        timestamp: Date.now(),
+        received: {
+            totalMessages: numOfMessagesReceived,
+            totalBytes: numOfBytesReceived,
+            newMessages: numOfMessagesReceived - lastReportedNumOfMessagesReceived,
+            newBytes: numOfBytesReceived - lastReportedNumofBytesReceived,
+        },
+        sent: {
+            totalMessages: numOfMessagesSent,
+            totalBytes: numOfBytesSent,
+            newMessages: numOfMessagesSent - lastReportedNumOfMessagesSent,
+            newBytes: numOfBytesSent - lastReportedNumofBytesSent,
+        },
+        neighbors: Object.keys(connections),
+        connections: {
+            total: Object.values(connections).length,
+            connectionStates: Object.values(connections).map((c) => c.connectionState),
+            iceConnectionStates: Object.values(connections).map((c) => c.iceConnectionState),
+            iceGatheringStates: Object.values(connections).map((c) => c.iceGatheringState),
+            signalingStates: Object.values(connections).map((c) => c.signalingState),
+            stats: connectionStats,
+        },
+        dataChannels: {
+            total: Object.values(dataChannels).length,
+            readyStates: Object.values(dataChannels).map((d) => d.readyState),
+            bufferedAmount: Object.values(dataChannels).map((d) => d.bufferedAmount)
+        }
+    }) + '\n')
+
     lastReportedNumOfMessagesReceived = numOfMessagesReceived
     lastReportedNumofBytesReceived = numOfBytesReceived
     lastReportedNumOfMessagesSent = numOfMessagesSent
