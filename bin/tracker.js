fb506a926f10da8d94a53d952f6b8c8a8133e214
juslesan <santeri.juslenius@gmail.com>
2020-08-06 10:17:09 +0300

    logging

--- bin/tracker.js
+++ bin/tracker.js
@@ -28,6 +28,9 @@ program
     .description('Run tracker with reporting')
     .parse(process.argv)
 
+console.log(program.id)
+console.log(program.name)
+
 const id = program.id || `tracker-${program.port}`
 const name = program.trackerName || id
 
