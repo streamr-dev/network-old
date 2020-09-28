2a150b6aecd83700e511c8525a68ef53f62125df
juslesan <santeri.juslenius@gmail.com>
2020-09-28 12:39:54 +0300

    revert

--- src/connection/WebRtcEndpoint.js
+++ src/connection/WebRtcEndpoint.js
@@ -166,15 +166,11 @@ class WebRtcEndpoint extends EventEmitter {
                         console.warn(warnMessage)
                         this.emit(events.PEER_DISCONNECTED, this.peerInfos[targetPeerId])
                         this.emit(`disconnected:${targetPeerId}`, targetPeerId)
-                    } else {
-                        // delete this.flushTimeOutRefs[targetPeerId]
-                        this._attemptToFlushMessages(targetPeerId)
-                    }
-                    // else if (this.flushTimeOutRefs[targetPeerId] === null) {
-                    //     this.flushTimeOutRefs[targetPeerId] = setTimeout(() => {
-                    //         delete this.flushTimeOutRefs[targetPeerId]
-                    //         this._attemptToFlushMessages(targetPeerId)
-                    //     }, 10)
+                    } else if (this.flushTimeOutRefs[targetPeerId] === null) {
+                        this.flushTimeOutRefs[targetPeerId] = setTimeout(() => {
+                            delete this.flushTimeOutRefs[targetPeerId]
+                            this._attemptToFlushMessages(targetPeerId)
+                        }, 100)
                     return
                 }
             }
