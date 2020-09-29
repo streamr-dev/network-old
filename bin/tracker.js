eeb5a3c7bc7d3f373caae4668e1c5e5823f75798
mirpo <7105861+mirpo@users.noreply.github.com>
2020-09-29 11:24:02 +0300

    Special for Tim

--- bin/tracker.js
+++ bin/tracker.js
@@ -93,7 +93,7 @@ async function main() {
 
                 // output to console
                 if (program.metrics) {
-                    logger.info(JSON.stringify(metrics, null, 3))
+                    logger.info(JSON.stringify(metrics, null, 4))
                 }
             }, program.metricsInterval)
         }
