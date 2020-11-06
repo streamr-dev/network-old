ed3421043630108b81f401a2764dd57f78917055
Eric Andrews <eric.andrews@streamr.com>
2020-11-06 16:01:51 +0200

    remove unused methos from PromiseTools

--- src/helpers/PromiseTools.js
+++ src/helpers/PromiseTools.js
@@ -1,36 +1,21 @@
-const promiseTimeout = (ms, promise) => {
-    const timeout = new Promise((resolve, reject) => {
-        const to = setTimeout(() => {
-            // eslint-disable-next-line prefer-promise-reject-errors
-            reject('timed out in ' + ms + 'ms.')
+const promiseTimeout = (ms, givenPromise) => {
+    const timeoutPromise = new Promise((resolve, reject) => {
+        const timeoutRef = setTimeout(() => {
+            reject(new Error('timed out in ' + ms + 'ms.'))
         }, ms)
 
         // Clear timeout if promise wins race
-        Promise.resolve(promise)
-            .then(() => clearInterval(to))
-            .catch((e) => console.error(e))
+        givenPromise
+            .finally(() => clearTimeout(timeoutRef))
+            .catch(() => null)
     })
 
     return Promise.race([
-        promise,
-        timeout
+        givenPromise,
+        timeoutPromise
     ])
 }
 
-const sleep = (ms) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms)
-    })
-}
-
-const immediateSleep = () => {
-    return new Promise((resolve) => {
-        setImmediate(resolve)
-    })
-}
-
 module.exports = {
-    promiseTimeout,
-    sleep,
-    immediateSleep
+    promiseTimeout
 }
