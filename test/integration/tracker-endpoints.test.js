a4d93175163bc67449c08001ff6e3f1075e3497f
Eric Andrews <eric.andrews@streamr.com>
2020-12-17 15:21:53 +0200

    change /topology-union/ to /node-connections/

--- test/integration/tracker-endpoints.test.js
+++ test/integration/tracker-endpoints.test.js
@@ -161,8 +161,8 @@ describe('tracker endpoint', () => {
         })
     })
 
-    it('/topology-union/', async () => {
-        const [status, jsonResult] = await getHttp(`http://127.0.0.1:${trackerPort}/topology-union/`)
+    it('/node-connections/', async () => {
+        const [status, jsonResult] = await getHttp(`http://127.0.0.1:${trackerPort}/node-connections/`)
         expect(status).toEqual(200)
         expect(jsonResult).toEqual({
             'node-1': ['node-2'],
