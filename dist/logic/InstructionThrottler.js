995c234df4701ed9d9ee6fe04cff79adab893512
juslesan <santeri.juslenius@gmail.com>
2021-01-07 17:02:11 +0200

    use instruction counters in throttler

--- dist/logic/InstructionThrottler.js
+++ dist/logic/InstructionThrottler.js
@@ -13,62 +13,76 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.InstructionThrottler = void 0;
+const cancelable_promise_1 = require("cancelable-promise");
 const identifiers_1 = require("../identifiers");
 const logger_1 = __importDefault(require("../helpers/logger"));
 const logger = logger_1.default('streamr:logic:InstructionThrottler');
-function wait(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
 /**
  * InstructionThrottler makes sure that
- *  1. only 100 instructions are handled per second
+ *  1. no more than one instruction is handled at a time
  *  2. any new instructions arriving while an instruction is being handled are queued in a
  *     way where only the most latest instruction per streamId is kept in queue.
  */
 class InstructionThrottler {
     constructor(handleFn) {
         this.queue = {}; // streamId => instructionMessage
+        this.instructionCounter = {}; // streamId => counter
         this.handling = false;
+        this.ongoingPromise = null;
         this.handleFn = handleFn;
     }
     add(instructionMessage, trackerId) {
-        this.queue[identifiers_1.StreamIdAndPartition.fromMessage(instructionMessage).key()] = {
-            instructionMessage,
-            trackerId
-        };
-        if (!this.handling) {
-            this.invokeHandleFnWithLock().catch((err) => {
-                logger.warn("Error handling instruction %s", err);
-            });
+        const streamId = identifiers_1.StreamIdAndPartition.fromMessage(instructionMessage).key();
+        if (!this.instructionCounter[streamId] || this.instructionCounter[streamId] <= instructionMessage.counter) {
+            this.instructionCounter[streamId] = instructionMessage.counter;
+            this.queue[identifiers_1.StreamIdAndPartition.fromMessage(instructionMessage).key()] = {
+                instructionMessage,
+                trackerId
+            };
+            if (!this.handling) {
+                this.invokeHandleFnWithLock().catch((err) => {
+                    logger.warn("Error handling instruction %s", err);
+                });
+            }
         }
     }
     removeStreamId(streamId) {
         delete this.queue[streamId];
+        delete this.instructionCounter[streamId];
     }
     isIdle() {
         return !this.handling;
     }
     reset() {
         this.queue = {};
+        this.instructionCounter = {};
+        if (this.ongoingPromise) {
+            this.ongoingPromise.cancel();
+        }
     }
     invokeHandleFnWithLock() {
         return __awaiter(this, void 0, void 0, function* () {
             const streamIds = Object.keys(this.queue);
-            if (streamIds.length > 0) {
-                const streamId = streamIds[0];
-                const { instructionMessage, trackerId } = this.queue[streamId];
-                delete this.queue[streamId];
-                this.handling = true;
-                yield wait(10);
+            const streamId = streamIds[0];
+            const { instructionMessage, trackerId } = this.queue[streamId];
+            delete this.queue[streamId];
+            this.handling = true;
+            try {
+                this.ongoingPromise = cancelable_promise_1.cancelable(this.handleFn(instructionMessage, trackerId));
+                yield this.ongoingPromise;
+            }
+            catch (err) {
+                logger.warn('InstructionMessage handling threw error %s', err);
+                logger.warn(err);
+            }
+            finally {
+                this.ongoingPromise = null;
                 if (this.isQueueEmpty()) {
                     this.handling = false;
                 }
-                this.handleFn(instructionMessage, trackerId).catch((err) => {
-                    logger.warn("Error handling instruction %s", err);
-                });
-                this.invokeHandleFnWithLock().catch((err) => {
-                    logger.warn("Error handling instruction %s", err);
-                });
+                else {
+                    this.invokeHandleFnWithLock();
+                }
             }
         });
     }
