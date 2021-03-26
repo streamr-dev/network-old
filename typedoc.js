d45ff8c0c6dda4bd0c1c96bb54ddf11b214cd624
Tim Oxley <secoif@gmail.com>
2021-03-26 14:47:22 -0400

    docs: Generate docs with typedoc.

---
+++ typedoc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entryPoints: [
+        'src/composition.ts',
+    ],
+    tsconfig: 'tsconfig.json',
+    readme: false,
+    excludeInternal: true,
+    includeVersion: true,
+}
