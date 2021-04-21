99235cfc665c94bd44af730dfae12dc1a74a9a4f
juslesan <santeri.juslenius@gmail.com>
2021-04-21 13:46:45 +0300

    24.1.9-emu.11

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -137,7 +137,7 @@ export class Connection {
         this.onBufferLow = onBufferLow
         this.onBufferHigh = onBufferHigh
         nodeDataChannel.setSctpSettings({
-            congestionControlModule: 2
+            congestionControlModule: 3
         })
     }
 
