cb58fbce6eeadcb2d0886f62da87ab994672a364
Eric Andrews <harbu88@hotmail.com>
2021-04-30 11:10:41 +0300

    documents

--- src/NetworkNode.ts
+++ src/NetworkNode.ts
@@ -32,10 +32,22 @@ export class NetworkNode extends Node {
         })
     }
 
+    /**
+     * Publish a message in a "fire-and-forget" manner, attempting to propagate message to neighbors if available.
+     * @param streamMessage message to be published
+     * @returns number of neighbors message was forwarded to
+     */
     publish(streamMessage: MessageLayer.StreamMessage): number {
         return this.onDataReceived(streamMessage)
     }
 
+    /**
+     * Publish a message waiting first for at least `minNeighbors` neighbors to be available for a propagation attempt.
+     * @param streamMessage message to be published
+     * @param minNeighbors minimum number of neighbors that need be available
+     * @param timeoutInMs time (in milliseconds) to wait for neighbors to be available
+     * @returns number of neighbors message was forwarded to, or, rejection on timeout
+     */
     async asyncPublish(
         streamMessage: MessageLayer.StreamMessage,
         minNeighbors = 1,
@@ -48,14 +60,24 @@ export class NetworkNode extends Node {
         return this.publish(streamMessage)
     }
 
+    /**
+     * Set callback function that is invoked each time a new unique message is encountered.
+     * @param cb callback function
+     */
     addMessageListener(cb: (msg: MessageLayer.StreamMessage) => void): void {
         this.on(NodeEvent.UNSEEN_MESSAGE_RECEIVED, cb)
     }
 
+    /**
+     * Join a stream
+     */
     subscribe(streamId: string, streamPartition: number): void {
         this.subscribeToStreamIfHaveNotYet(new StreamIdAndPartition(streamId, streamPartition))
     }
 
+    /**
+     * Leave a stream
+     */
     unsubscribe(streamId: string, streamPartition: number): void {
         this.unsubscribeFromStream(new StreamIdAndPartition(streamId, streamPartition))
     }
@@ -115,10 +137,24 @@ export class NetworkNode extends Node {
         return this.requestResend(request, null)
     }
 
+
+    /**
+     * Get number of neighbors for a given StreamPart
+     * @param streamId
+     * @param streamPartition
+     */
     public getNumberOfNeighbors(streamId: string, streamPartition: number): number {
         return this.getNeighborsFor(new StreamIdAndPartition(streamId, streamPartition)).length
     }
 
+    /**
+     * Wait until at least the given number of neighbors are available on a stream.
+     * @param streamId
+     * @param streamPartition
+     * @param minNeighbors minimum number of neighbors to a wait for
+     * @param timeoutInMs timeout (in milliseconds) after which to give up waiting
+     * @returns nubmer of neighbors available
+     */
     public waitForNeighbors(
         streamId: string,
         streamPartition: number,
