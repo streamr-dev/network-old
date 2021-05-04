f9ccdf50a5c1cc35fde037f4e24a064d01f6eb91
juslesan <santeri.juslenius@gmail.com>
2021-05-04 15:49:41 +0300

    test ci

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -446,12 +446,11 @@ export class Connection extends ConnectionEmitter {
         this.paused = false
         this.dataChannelEmitter = DataChannelEmitter(dataChannel)
         dataChannel.setBufferedAmountLowThreshold(this.bufferThresholdLow)
-        if (this.isOffering) {
-            this.dataChannelEmitter.on('open', () => {
-                this.logger.debug('dc.onOpen')
-                this.openDataChannel(dataChannel)
-            })
-        }
+        this.dataChannelEmitter.on('open', () => {
+            this.logger.debug('dc.onOpen')
+            this.openDataChannel(dataChannel)
+        })
+
         this.dataChannelEmitter.on('closed', () => {
             this.logger.debug('dc.onClosed')
             this.close()
